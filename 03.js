f39a541eefe2b1f9f57c82069bdff0dc22312223
KeishiKITAGAWA <kaynekk0115@gmail.com>
2019-06-29 16:34:00 +0900

    6/28 課題

--- 03.js
+++ 03.js
@@ -1,19 +1,33 @@
 // 元素記号
 
-/* let periTab = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can"
+let periTab = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can";
+
+let ans = [];
+
+let one = [1, 5, 6, 7, 8, 9, 15, 16, 19];
+
+let two = [2, 3, 4, 10, 11, 13, 14, 17, 18, 20];
 
 let spliTab = periTab.split(' ');
+// スペースで区切り、20個のStringを収納したArrayにする
 
-for (let i = 0; i <= 19; i++) {
-    let index = spliTab.indexOf(i)
-    if (index = )
-} */
-
-for (var i = 1; i <= 5; i++) {
-    if (i = 1) {
-        console.log("${i}は奇数");
-    } else {
-        console.log("${i}は偶数");
-    }
+for (let i = 0; i < spliTab.length ; i++) {
+    ans[String(i + 1)] = spliTab[i].slice(0, 3);
 }
+// 各要素の先頭の3文字を切り取る
+
+for (let i in one) {
+    ans[one[i]] = ans[one[i]].slice(0, 1);
+}
+// oneに属している数字番目の文字の先頭だけを切り取る
+
+ans[12] = ans[12].replace("i", "");
+// 問題の不備？　欲しいのは Mi ではなく Mg なので Mg が出力されるようにする　最初に全て3文字切り取ったのはこれのため
+
+for (let i in two) {
+    ans[two[i]] = ans[two[i]].slice(0, 2);
+} 
+// twoに属している数字番目の単語は先頭から2文字切り取る
+
+console.log(ans);
 
--- README.md
+++ README.md
@@ -70,9 +70,11 @@ FizzBuzz
 ### 問題文
 
 引用元: 【タイトルとリンクを書く】
-練習問題 http://vipprog.net/wiki/exercise.html
+言語処理100本ノック　http://www.cl.ecei.tohoku.ac.jp/nlp100/#sec04
 【ここに問題を書き写す】
 
+04. 元素記号
+"Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
 
 
 
